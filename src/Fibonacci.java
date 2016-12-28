792e64bd6ae894c365611b87fe45121696727565
Stella FOUENANG <Stella FOUENANG>
2016-12-28 04:04:07 +0100

    Ajout methode recursive et tests

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -5,7 +5,55 @@ import java.util.Scanner;
  */
 public class Fibonacci {
 
-//    public static void main(String[] args){
+
+
+    /*************************** First Method of FIBONACCI : ITERATIVE ********************************/
+
+    public static int calc_fibonacci (int input){
+
+        int index;
+        int current_fibonacci = 0;
+        int previous_n_1 = 1;
+        int previous_n_2 = 0;
+
+
+        System.out.println("Les"+" "+ input +" premiers elements de la suite de Fibonacci sont:");
+
+        for (index = 0 ; index <= input; index ++){
+
+            if (index == 1){
+                current_fibonacci = 1;
+            }
+
+            if(index > 1) {
+                current_fibonacci = previous_n_1 + previous_n_2;
+                previous_n_2 = previous_n_1;
+                previous_n_1 = current_fibonacci;
+            }
+
+            System.out.println(current_fibonacci+"-");
+        }
+        return current_fibonacci;
+
+
+    }
+
+    /*************************** Second Method of FIBONACCI : RECURSIVE ********************************/
+
+    public static int calc_fibonacci2(int input){
+
+        int param_fibonacci;
+
+        if(input <= 1){
+            return input;
+        }
+        else
+            return calc_fibonacci2(input-1) + calc_fibonacci2(input - 2);
+
+    }
+
+
+ //               public static void main(String[] args){
 //
 //        int index;
 //        int input_number;
@@ -37,38 +85,5 @@ public class Fibonacci {
 //
 //
 //    }
-
-    /*************************** First Method of FIBONACCI ********************************/
-
-    public static int calc_fibonacci (int input){
-
-        int index;
-        int current_fibonacci = 0;
-        int previous_n_1 = 1;
-        int previous_n_2 = 0;
-
-
-        System.out.println("Les"+" "+ input +" premiers elements de la suite de Fibonacci sont:");
-
-        for (index = 0 ; index <= input; index ++){
-
-            if (index == 1){
-                current_fibonacci = 1;
-            }
-
-            if(index > 1) {
-                current_fibonacci = previous_n_1 + previous_n_2;
-                previous_n_2 = previous_n_1;
-                previous_n_1 = current_fibonacci;
-            }
-
-            System.out.println(current_fibonacci+"-");
-        }
-        return current_fibonacci;
-
-
-    }
-
-    /*************************** Second Method of FIBONACCI ********************************/
 
 }
--- test/TestFibonacci.java
+++ test/TestFibonacci.java
@@ -6,10 +6,10 @@ import org.junit.Test;
  */
 public class TestFibonacci {
 
-//    @Test
-//    public void test_should_return_the_lastNumberOfFibonacci_for_an_input(){
-//        Assert.assertEquals(13, Fibonacci.calc_fibonacci(6));
-//    }
+    @Test
+    public void test_should_return_the_lastNumberOfFibonacci_for_an_input(){
+        Assert.assertEquals(8, Fibonacci.calc_fibonacci(6));
+    }
 
     @Test
     public void test_should_return_fibonacci_calc() {
@@ -18,5 +18,13 @@ public class TestFibonacci {
         Assert.assertEquals(1, Fibonacci.calc_fibonacci(1));
         Assert.assertEquals(1, Fibonacci.calc_fibonacci(2));
         Assert.assertEquals(8, Fibonacci.calc_fibonacci(6));
+    }
+
+    @Test
+    public void test_should_return_fibonacci_calc2(){
+        Assert.assertEquals(0, Fibonacci.calc_fibonacci2(0));
+//        Assert.assertEquals(1, Fibonacci.calc_fibonacci2(1));
+//        Assert.assertEquals(1, Fibonacci.calc_fibonacci2(2));
+//        Assert.assertEquals(8, Fibonacci.calc_fibonacci2(6));
     }
 }
